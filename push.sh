108a7e0dbb1d6d7bd2755093f7dd0b07631d6a1d
Andy Nicholson <andrew@anicholson.net>
2018-05-12 21:03:36 +1000

    Attempt to multi-stage sync to add cache-control

--- push.sh
+++ push.sh
@@ -2,4 +2,9 @@
 
 cd frontend/build
 
-aws s3 sync $(pwd) s3://andrewdotni.ch/ --include "*"
+aws s3 sync --delete --exclude static/* $(pwd) s3://andrewdotni.ch/ --include
+aws s3 sync --delete --cache-control "max-age=31536000" static/* s3://andrewdotni.ch/static
+
+#aws s3 sync --delete --exclude assets/* build/ "s3://${BUCKET_NAME}"
+#      - aws s3 sync --delete --cache-control "max-age=31536000" build/assets "s3://${BUCKET_NAME}/assets"
+
