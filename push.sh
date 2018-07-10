9e369b226d069f289d8fb4caa6e9106c31395b46
Andy Nicholson <andrew@anicholson.net>
2018-07-10 21:55:08 +1000

    Fix the build script

--- push.sh
+++ push.sh
@@ -2,8 +2,8 @@
 
 cd frontend/build
 
-aws s3 sync --delete --exclude assets/* $(pwd) s3://andrewdotni.ch/
-aws s3 sync --delete --cache-control "max-age=31536000" assets/* s3://andrewdotni.ch/assets
+aws s3 sync --delete --exclude assets/ $(pwd) s3://andrewdotni.ch/
+aws s3 sync --delete --cache-control "max-age=31536000" assets/ s3://andrewdotni.ch/assets
 
 #aws s3 sync --delete --exclude assets/* build/ "s3://${BUCKET_NAME}"
 #      - aws s3 sync --delete --cache-control "max-age=31536000" build/assets "s3://${BUCKET_NAME}/assets"
